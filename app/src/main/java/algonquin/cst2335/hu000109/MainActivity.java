b4a32163096e7da0b18a25bf204fac62ccbee338
Danni Hu <danniyh@outlook.com>
2024-01-17 09:42:05 -0500

    xinzeng

---
+++ app/src/main/java/algonquin/cst2335/hu000109/MainActivity.java
@@ -0,0 +1,15 @@
+package algonquin.cst2335.hu000109;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //测试一个没用的提交
+}

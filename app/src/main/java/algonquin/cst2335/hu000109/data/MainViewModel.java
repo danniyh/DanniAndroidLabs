884d62aeaea69654efe7994d11ed1fb0ea3fba5a
Danni Hu <danniyh@outlook.com>
2024-01-31 16:08:55 -0500

    Finished week 2

--- app/src/main/java/algonquin/cst2335/hu000109/data/MainViewModel.java
+++ app/src/main/java/algonquin/cst2335/hu000109/data/MainViewModel.java
@@ -1,11 +1,26 @@
 package algonquin.cst2335.hu000109.data;
 
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+public class MainViewModel extends ViewModel {
 
-
-public class MainViewModel extends ViewModel{
-
+    //4
     public MutableLiveData<String> editString = new MutableLiveData<>();
-    public String editString;
+
+
+    //5
+    private MutableLiveData<Boolean> isSelected = new MutableLiveData<>();
+
+    public MutableLiveData<Boolean> getIsSelected() {
+        return isSelected;
+    }
+
+    public void setIsSelected(boolean preference) {
+        isSelected.setValue(preference);
+    }
+
+
+    //6
+    private MutableLiveData<Boolean> imageClicked = new MutableLiveData<>();
+
 }
--- app/src/main/java/algonquin/cst2335/hu000109/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/hu000109/ui/MainActivity.java
@@ -1,44 +1,94 @@
 package algonquin.cst2335.hu000109.ui;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+//2
+
+
+//2
+import algonquin.cst2335.hu000109.databinding.ActivityMainBinding;
+
+//4
 import algonquin.cst2335.hu000109.data.MainViewModel;
 
 import androidx.lifecycle.ViewModelProvider;
-//下面都是新增加的
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import algonquin.cst2335.hu000109.R;
-import algonquin.cst2335.hu000109.databinding.ActivityMainBinding;
+
+
+//5
+import android.widget.Toast;
+
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private MainViewModel model;
-    private ActivityMainBinding variableBinding;
 
+    private ActivityMainBinding variableBinding;//2
+    private MainViewModel model;//4
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-        model = new ViewModelProvider(this).get(MainViewModel.class);
-
+        //2
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
+
 
         //3+4
         if(variableBinding.mybutton != null) variableBinding.mybutton.setOnClickListener(click ->
         {
             model.editString.postValue(variableBinding.myedittext.getText().toString());
 
-            model.editString.observe(this, s -> {
-                variableBinding.textview.setText("Your edit text has: " + s);
+            model.editString.observe(this,s -> {
+                variableBinding.textview.setText( "Your edit text has: " + s);
             });
+
+
         });
-        }
+
+        //4
+        model = new ViewModelProvider(this).get(MainViewModel.class);
+
+
+        //5
+
+
+        model.getIsSelected().observe(this, selected ->{
+            variableBinding.checkBox.setChecked(selected);
+            variableBinding.radioButton.setChecked(selected);
+            variableBinding.switchButton.setChecked(selected);
+            showToast("The value is now: " + selected);
+        });
+
+
+
+        variableBinding.switchButton.setOnCheckedChangeListener((buttonView, isChecked) -> model.getIsSelected().postValue(isChecked));
+
+        variableBinding.radioButton.setOnCheckedChangeListener((buttonView, isChecked) -> model.setIsSelected(isChecked));
+
+        variableBinding.checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> model.setIsSelected(isChecked));
+
+
+        //6
+        variableBinding.imageView.setOnClickListener(v -> showToast("ImageView clicked"));
+
+        variableBinding.myimagebutton.setOnClickListener(v->
+        {
+            showToast("ImageButton clicked");
+            int width = v.getWidth();
+            int height = v.getHeight();
+            showToast("The width = " + width + " and height = " + height);
+        });
+    }
+
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
